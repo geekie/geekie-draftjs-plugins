e8544a84b195c7cc3438f5f2816c368f35474932
Mateus Barros <mateus.rodrigues@geekie.com.br>
2022-08-22 10:27:27 -0300

    feat: adds https on urls by default

--- packages/linkify/src/source/source.tsx
+++ packages/linkify/src/source/source.tsx
@@ -7,8 +7,6 @@ type State = {
   content: string;
 };
 
-
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 class LinkSource extends Component<any, State> {
   inputContentRef?: HTMLInputElement | null;
@@ -21,7 +19,7 @@ class LinkSource extends Component<any, State> {
     const selectionText = getSelectionText(editorState);
 
     const state = {
-      url: "",
+      url: '',
       content: selectionText,
     };
 
@@ -39,7 +37,7 @@ class LinkSource extends Component<any, State> {
     this.onChangeContent = this.onChangeContent.bind(this);
   }
 
-  onConfirm(e: React.FormEvent<HTMLFormElement>) : void {
+  onConfirm(e: React.FormEvent<HTMLFormElement>): void {
     const { editorState, onComplete } = this.props;
     const { url, content } = this.state;
     e.preventDefault();
@@ -47,78 +45,77 @@ class LinkSource extends Component<any, State> {
     onComplete(newEditorState);
   }
 
-  onRequestClose(e: React.SyntheticEvent) : void {
+  onRequestClose(e: React.SyntheticEvent): void {
     const { onClose } = this.props;
     e.preventDefault();
     onClose();
   }
 
-  onChangeURL(e: React.ChangeEvent<HTMLInputElement>) : void {
+  onChangeURL(e: React.ChangeEvent<HTMLInputElement>): void {
     if (e.target instanceof HTMLInputElement) {
       const url = e.target.value;
       this.setState({ url });
     }
   }
 
-  onChangeContent(e: React.ChangeEvent<HTMLInputElement>) : void {
+  onChangeContent(e: React.ChangeEvent<HTMLInputElement>): void {
     if (e.target instanceof HTMLInputElement) {
       const content = e.target.value;
       this.setState({ content });
     }
   }
   //todo tipar
-  render() : any {
+  render(): any {
     const { textDirectionality } = this.props;
     const { url, content } = this.state;
-    const styleForm : CSSProperties = {
-          pointerEvents: "all",
-          border: "1px solid #F1F1F1",
-          boxShadow: "2px 3px 8px rgba(0, 0, 0, 0.3)",
-          fontFamily: "Lato, Arial",
-          width: 276,
-          textAlign: "center",
+    const styleForm: CSSProperties = {
+      pointerEvents: 'all',
+      border: '1px solid #F1F1F1',
+      boxShadow: '2px 3px 8px rgba(0, 0, 0, 0.3)',
+      fontFamily: 'Lato, Arial',
+      width: 276,
+      textAlign: 'center',
     };
-    const styleLabel : CSSProperties = {
-          display: "block",
-          fontWeight: 700,
-          fontFamily: "Lato, Arial",
-          fontSize:14,
-          lineHeight:"17px",
-          marginTop: "15px",
-          marginLeft: "16px",
-          textAlign: "left",
+    const styleLabel: CSSProperties = {
+      display: 'block',
+      fontWeight: 700,
+      fontFamily: 'Lato, Arial',
+      fontSize: 14,
+      lineHeight: '17px',
+      marginTop: '15px',
+      marginLeft: '16px',
+      textAlign: 'left',
     };
-    const styleInput : CSSProperties = {
-          display: "block",
-          marginTop: "6px",
-          border: "1px solid #D3D7DE",
-          borderRadius: "4px",
-          width:244,
-          height:32,
-          marginLeft: "16px",
+    const styleInput: CSSProperties = {
+      display: 'block',
+      marginTop: '6px',
+      border: '1px solid #D3D7DE',
+      borderRadius: '4px',
+      width: 244,
+      height: 32,
+      marginLeft: '16px',
     };
-    const styleButton : CSSProperties = {
-        display: "inline-block",
-        width: 74,
-        height: 30,
-        margin: "10px",
-        backgroundColor: "#FFFFFF",
-        border: "1px solid #F1F1F1",
+    const styleButton: CSSProperties = {
+      display: 'inline-block',
+      width: 74,
+      height: 30,
+      margin: '10px',
+      backgroundColor: '#FFFFFF',
+      border: '1px solid #F1F1F1',
     };
 
-      
     return (
       <form
-        dir={textDirectionality === "RTL" ? "rtl" : undefined}
+        dir={textDirectionality === 'RTL' ? 'rtl' : undefined}
         className="LinkSource"
         onSubmit={this.onConfirm}
         style={styleForm}
       >
-        
-        <label 
-          className={"form-field-content"} 
-          style={styleLabel} 
-          htmlFor={"geekie-link-id-content"} >
+        <label
+          className={'form-field-content'}
+          style={styleLabel}
+          htmlFor={'geekie-link-id-content'}
+        >
           Texto
         </label>
         <input
@@ -133,12 +130,11 @@ class LinkSource extends Component<any, State> {
           style={styleInput}
         />
 
-        
-
-        <label 
-          className={"form-field-url"} 
-          style={styleLabel} 
-          htmlFor={"geekie-link-id-url"} >
+        <label
+          className={'form-field-url'}
+          style={styleLabel}
+          htmlFor={'geekie-link-id-url'}
+        >
           Link
         </label>
         <input
@@ -153,59 +149,68 @@ class LinkSource extends Component<any, State> {
           style={styleInput}
         />
 
-
-        <button type="submit" style = {styleButton}>Ok</button>
-        <button style = {styleButton}>Cancelar</button>
+        <button type="submit" style={styleButton}>
+          Ok
+        </button>
+        <button style={styleButton}>Cancelar</button>
       </form>
     );
   }
 }
 
-
-
-function editLinkEntity(content:string, url:string, editorState:EditorState) : EditorState {
+function editLinkEntity(
+  content: string,
+  url: string,
+  editorState: EditorState
+): EditorState {
   let selection = editorState.getSelection();
   let newEditorState = editorState;
-  newEditorState = editText(newEditorState,selection,content);
+  newEditorState = editText(newEditorState, selection, content);
 
   //seleciona texto se foi alterado
-  const start = Math.min(selection.getStartOffset(),selection.getEndOffset());
+  const start = Math.min(selection.getStartOffset(), selection.getEndOffset());
   const end = start + content.length;
-  
+
   selection = selection.merge({
     anchorOffset: start,
     focusOffset: end,
     isBackward: false,
   });
-  
-  newEditorState = addLink(newEditorState,selection,url);
+
+  newEditorState = addLink(newEditorState, selection, url);
   return newEditorState;
 }
 
-function addLink(editorState:EditorState, selection:SelectionState,  url:string) : EditorState {
+function addLink(
+  editorState: EditorState,
+  selection: SelectionState,
+  url: string
+): EditorState {
   let newEditorState = editorState;
   const contentState = editorState.getCurrentContent();
-  if (url!=="") {//TODO(tumais) - validar url e complementar com https:// os casos necessários
-    const state = contentState.createEntity(
-      ENTITY_TYPE.LINK,
-      "MUTABLE",
-      { url }
-    );
+  if (url !== '') {
+    let finalUrl = url;
+    if (!url.startsWith('http://') || !url.startsWith('https://'))
+      finalUrl = `https://${url}`;
+
+    const state = contentState.createEntity(ENTITY_TYPE.LINK, 'MUTABLE', {
+      url: finalUrl,
+    });
 
     const entityKey = contentState.getLastCreatedEntityKey();
 
-    const newContentState = Modifier.applyEntity(
-      state,
-      selection,
-      entityKey
+    const newContentState = Modifier.applyEntity(state, selection, entityKey);
+
+    newEditorState = EditorState.push(
+      editorState,
+      newContentState,
+      'apply-entity'
     );
-    
-    newEditorState = EditorState.push(editorState, newContentState, "apply-entity");
   }
   return newEditorState;
 }
 
-function getSelectionText(editorState:EditorState) : string{
+function getSelectionText(editorState: EditorState): string {
   const selection = editorState.getSelection();
   const currentContent = editorState.getCurrentContent();
   const anchorKey = selection.getAnchorKey();
@@ -216,10 +221,22 @@ function getSelectionText(editorState:EditorState) : string{
   return selectedText;
 }
 
-function editText(editorState:EditorState, selection:SelectionState, content:string) : EditorState{
+function editText(
+  editorState: EditorState,
+  selection: SelectionState,
+  content: string
+): EditorState {
   const contentState = editorState.getCurrentContent();
-  const newContentState = Modifier.replaceText(contentState, selection, content);
-  const newEditorState = EditorState.push(editorState, newContentState, 'replace-text' as EditorChangeType);
+  const newContentState = Modifier.replaceText(
+    contentState,
+    selection,
+    content
+  );
+  const newEditorState = EditorState.push(
+    editorState,
+    newContentState,
+    'replace-text' as EditorChangeType
+  );
   return newEditorState;
 }
 
