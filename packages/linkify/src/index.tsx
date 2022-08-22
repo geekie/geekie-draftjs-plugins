63eb69d69b6c69a2d5bf1182ba635b33bf3c3313
Leonardo Pádua <leonardocpadua7@gmail.com>
2022-08-22 15:46:41 -0300

    fix

--- packages/linkify/src/index.tsx
+++ packages/linkify/src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import { ENTITY_TYPE } from 'draftail';
 import source from './source/source';
 import decorator from './decorator/decorator';
 import LinkIcon from './linkIcon';
 
 //todo tipar
-export default (config: any) => ({
-  entityType: {
-    icon: LinkIcon,
-    //onPaste: onPaste,
-    source,
-    decorator,
-    attributes: ['url'],
-    schemes: ['http:', 'https:'],
-    type: ENTITY_TYPE.LINK,
-  },
-});
+export default (config: any) => {
+  return {
+    entityType: {
+      icon: LinkIcon,
+      //onPaste: onPaste,
+      source,
+      decorator,
+      attributes: ['url'],
+      schemes: ['http:', 'https:'],
+      type: ENTITY_TYPE.LINK,
+    },
+  };
+};
