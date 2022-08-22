120dfed37bb3a4dbbe56b0eaca3bb2c2839b5741
Osvaldo Zonetti <zonetg3@gmail.com>
2022-08-22 17:47:55 -0300

    refactor

--- packages/image/src/index.tsx
+++ packages/image/src/index.tsx
@@ -1,11 +1,12 @@
 import React, { ComponentType, ReactElement } from 'react';
 import { ContentBlock, EditorState } from 'draft-js';
-import { EditorPlugin } from '@draft-js-plugins/editor';
+import { EditorPlugin } from '../../editor/src';
 import addImage, { IMAGE_ENTITY_TYPE } from './modifiers/addImage';
 import ImageComponent, { ImageProps } from './Image';
 import { control, SelectImageControl } from './SelectImageControl';
 import { defaultTheme } from './theme';
-import { setFileLimitation } from './register';
+import { getUploadImage, setFileLimitation } from './register';
+import { getAcceptableSize, getHeightAndWidthFromDataUrl, isValidImageSize, isValidImageType } from './utils';
 
 export { registerUploadImageTask, setFileLimitation } from './register';
 
@@ -24,8 +25,6 @@ export type ImageEditorPlugin = EditorPlugin & {
     type: string;
   };
 };
-
-const ACCEPTED_MIMES = ['image/png', 'image/jpeg', 'image/gif'];
 
 const createSetResizeData =
   (
@@ -69,52 +68,37 @@ export default (config: ImagePluginConfig = {}): ImageEditorPlugin => {
 
   return {
     blockRendererFn: (block, { getEditorState, setEditorState }) => {
-      if (block.getType() === 'atomic') {
-        const contentState = getEditorState().getCurrentContent();
-        const entity = block.getEntityAt(0);
-        if (!entity) return null;
-        const type = contentState.getEntity(entity).getType();
-        const resizeData = contentState.getEntity(entity).getData();
-
-        if (type === IMAGE_ENTITY_TYPE) {
-          return {
-            component: ThemedImage,
-            editable: false,
-            props: {
-              resizeData,
-              setResizeData: createSetResizeData(block, {
-                getEditorState,
-                setEditorState,
-              }),
-            },
-          };
-        }
-        return null;
-      }
-
-      return null;
-    },
-    handlePastedFiles(e, editor) {
-      const editorState = editor.getEditorState();
-      if (e.length !== 1) return;
-      const image = e[0];
-      if (ACCEPTED_MIMES.indexOf(image.type) < 0) return;
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const url = event.target.result;
-        const newEditorState = addImage(editorState, url, {
-          width: 500,
-          height: 500
-        });
-        editor.setEditorState(newEditorState);
+      if (block.getType() !== 'atomic') return null;
+      const contentState = getEditorState().getCurrentContent();
+      const entity = block.getEntityAt(0);
+      if (!entity) return null;
+      const type = contentState.getEntity(entity).getType();
+      const resizeData = contentState.getEntity(entity).getData();
+      if (type !== IMAGE_ENTITY_TYPE) return null;
+      return {
+        component: ThemedImage,
+        editable: false,
+        props: {
+          resizeData,
+          setResizeData: createSetResizeData(block, {
+            getEditorState,
+            setEditorState,
+          }),
+        },
       };
-      reader.readAsDataURL(image);
+    },
+    handlePastedFiles: (files: File[], { getEditorState, setEditorState }) => {
+      if (!files.length || !isValidImageType(files[0]) || !isValidImageSize(files[0])) return 'handled';
+      getUploadImage()(files[0]).then(async (dataURL) => {
+        const originalSize = await getHeightAndWidthFromDataUrl(dataURL);
+        const acceptableSize = getAcceptableSize(originalSize);
+        setEditorState(addImage(getEditorState(), dataURL, acceptableSize));
+      })
+      return 'handled';
     },
     addImage,
     control,
-    entityType: {
-      type: IMAGE_ENTITY_TYPE,
-    },
+    entityType: { type: IMAGE_ENTITY_TYPE, }
   };
 };
 
