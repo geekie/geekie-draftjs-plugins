4cd17d308eada6b22da1fd042ed0dc9e014148f3
Osvaldo Zonetti <zonetg3@gmail.com>
2022-08-23 10:05:22 -0300

    feat: accept optional max_dimension when resizing images

--- packages/image/src/utils.ts
+++ packages/image/src/utils.ts
@@ -1,4 +1,4 @@
-import { pluginConfig } from "./register";
+import { pluginConfig } from './register';
 
 const ACCEPTED_MIMES = ['image/png', 'image/jpeg', 'image/gif'];
 const MAX_IMAGE_DIMENSION_PX = 500;
@@ -8,18 +8,27 @@ type Size = {
   height: number;
 };
 
-export const getAcceptableSize = ({width, height}: Size): Size => {
+type GetAcceptableSizeProps = Size & {
+  maxDimension?: number;
+};
+
+export const getAcceptableSize = ({
+  width,
+  height,
+  maxDimension,
+}: GetAcceptableSizeProps): Size => {
+  const max = maxDimension || MAX_IMAGE_DIMENSION_PX;
   let limitWidth = width;
   let limitHeight = height;
   const ratio = width / height;
   if (ratio > 1) {
-    if (width > MAX_IMAGE_DIMENSION_PX) {
-      limitWidth = MAX_IMAGE_DIMENSION_PX;
+    if (width > max) {
+      limitWidth = max;
       limitHeight = limitWidth / ratio;
     }
   } else {
-    if (height > MAX_IMAGE_DIMENSION_PX) {
-      limitHeight = MAX_IMAGE_DIMENSION_PX;
+    if (height > max) {
+      limitHeight = max;
       limitWidth = limitHeight * ratio;
     }
   }
@@ -38,6 +47,8 @@ export const getHeightAndWidthFromDataUrl = (dataURL: string): Promise<Size> =>
     img.src = dataURL;
   });
 
-export const isValidImageType = (file: File): boolean => ACCEPTED_MIMES.includes(file.type);
+export const isValidImageType = (file: File): boolean =>
+  ACCEPTED_MIMES.includes(file.type);
 
-export const isValidImageSize = (file: File): boolean => file.size <= pluginConfig.fileLimitation * 1024 * 1024;
+export const isValidImageSize = (file: File): boolean =>
+  file.size <= pluginConfig.fileLimitation * 1024 * 1024;
