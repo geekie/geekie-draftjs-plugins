e1006d81349783649337206e55a2af7ec78b72e6
Leonardo Pádua <leonardocpadua7@gmail.com>
2022-09-15 16:22:45 -0300

    rebasing

--- packages/katex/src/components/KatexBlock.tsx
+++ packages/katex/src/components/KatexBlock.tsx
@@ -39,6 +39,7 @@ type Props = {
 const KatexBlock = (props: Props): JSX.Element => {
   const { block, blockProps } = props;
   const { getEditorState, infoComponent, onRemove, onStartEdit, onFinishEdit } = blockProps;
+
   const data: KatexBlockState = getEditorState()
     .getCurrentContent()
     .getEntity(block.getEntityAt(0))
